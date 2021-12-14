82d9c3ea3340db949c9aa8aa336408cf9afd17af
Alex Egurbide <alexegurbide@gmail.com>
2021-12-14 12:43:25 -0800

    commit on 'main'

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
-const DappToken = artifacts.require('DappToken')
-const DaiToken = artifacts.require('DaiToken')
-const TokenFarm = artifacts.require('TokenFarm');
+const DappToken = artifacts.require("DappToken")
+const DaiToken = artifacts.require("DaiToken")
+const TokenFarm = artifacts.require("TokenFarm");
 
 module.exports = async function(deployer, network, accounts) {
   // Deploy Mock Dai Token
@@ -11,14 +11,14 @@ module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(DappToken)
   const dappToken = await DappToken.deployed()
 
-    // Deploy TokenFarm
-    await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
-    const tokenFarm = await TokenFarm.deployed()
+  // Deploy TokenFarm
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
+  const tokenFarm = await TokenFarm.deployed()
 
-    // Transfer all tokens to TokenFarm
-    await dappToken.transfer(tokenFarm.address, 1000000000000000000000000)
+  // Transfer all tokens to TokenFarm
+  await dappToken.transfer(tokenFarm.address, 1000000000000000000000000)
 
-     // Transfer 100 mock DAI tokens to Investor
-     await daiToken.transfer(account[1], 1000000000000000000000000)
-    };
+  // Transfer 100 mock DAI tokens to Investor
+  await daiToken.transfer(accounts[1], 100000000000000000000)
+  
 };
